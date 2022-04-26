7f18cc8b125552ea1a72e42860c8843a73b747bb
Niharika <niharika.dayama@coditas.com>
2022-04-26 17:46:29 +0530

    first-draft

---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+export const Images = {
+  moon: require("../assets/moon.png"),
+  direction: require("../assets/direction.png"),
+  water: require("../assets/water.png"),
+  rainy: require("../assets/rainy.png"),
+  cloud: require("../assets/cloud.png"),
+  next: require("../assets/next.png"),
+  location: require("../assets/location.png"),
+  search: require("../assets/search.png"),
+  clock: require("../assets/clock.png"),
+};
--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Images } from "../../constants/images";
 import "./style.scss";
 
 function Homepage() {
   return (
     <div className="container">
       <div className="card">
-        <div className="card-left">
-          <div className="left-row-1"></div>
+        <div className="card-style left">
+          <div className="left-row-1">
+            <img src={Images.moon} alt="moon" className="moon" />
+            <div>
+              <button className="fahrenheit">F</button>
+              <button className="celcius">C</button>
+            </div>
+          </div>
           <div className="left-row-2">
-            <p className="temp">27C</p>
+            <p className="temp">
+              27<span>&#8451;</span>
+            </p>
             <p className="date">17th jun' 21</p>
             <div className="date-time">
               <p>Thursday</p>
@@ -18,50 +27,118 @@ function Homepage() {
           </div>
           <div className="left-row-3">
             <div className="weather-detail">
-              <img src="" alt="Wind" />
+              <img src={Images.direction} alt="Wind" />
               <p>Wind</p>
               <p>10km/h</p>
             </div>
+            <p className="separator">|</p>
             <div className="weather-detail">
-              <img src="" alt="Hum" />
+              <img src={Images.water} alt="Hum" />
               <p>Hum</p>
               <p>54%</p>
             </div>
+            <p className="separator">|</p>
             <div className="weather-detail">
-              <img src="" alt="rain" />
+              <img src={Images.rainy} alt="rain" />
               <p>Rain</p>
               <p>0.2%</p>
             </div>
           </div>
-          <div className="left-row-4"></div>
+          <div className="left-row-4">
+            <div className="rectangle">
+              <div className="rectangle-info">
+                <p>
+                  24<span>&#8451;</span>
+                </p>
+                <img src={Images.cloud} alt="weather" />
+                <p>Tue</p>
+              </div>
+            </div>
+            <div className="rectangle">
+              <div className="rectangle-info">
+                <p>
+                  24<span>&#8451;</span>
+                </p>
+                <img src={Images.cloud} alt="weather" />
+                <p>Wed</p>
+              </div>
+            </div>
+            <div className="rectangle">
+              <div className="rectangle-info">
+                <p>
+                  24<span>&#8451;</span>
+                </p>
+                <img src={Images.cloud} alt="weather" />
+                <p>Thur</p>
+              </div>
+            </div>
+            <div className="rectangle">
+              <div className="rectangle-info">
+                <p>
+                  24<span>&#8451;</span>
+                </p>
+                <img src={Images.cloud} alt="weather" />
+                <p>Fri</p>
+              </div>
+            </div>
+          </div>
+          <button className="forward-arrow">
+            <img src={Images.next} alt="forward-arrow" />
+          </button>
         </div>
-        <div className="card-right">
+        <div className="card-style right">
           <div className="right-row-1">
-            <button type="submit">loc</button>
+            {/* <button type="submit" className="location-pin">
+              <img src={Images.location} alt="location-pin" />
+            </button> */}
             <form>
               <input type="text" />
-              <button type="submit"></button>
+              <button type="submit">
+                <img src={Images.search} alt="search-button" />
+              </button>
             </form>
           </div>
           <div className="right-row-2">
-            <div>
-              <p>Sunrise</p>
-              <div></div>
+            <div className="sun-details">
+              <p className="title">Sunrise</p>
+              <div className="clock">
+                <div className="clock-img">
+                  <img src={Images.clock} alt="clock" />
+                </div>
+                <p className="t-1">11:24</p>
+                <p className="t-2">11:45</p>
+              </div>
             </div>
-            <div>
-              <p>Sunset</p>
-              <div></div>
+            <div className="sun-details">
+              <p className="title">Sunset</p>
+              <div className="clock">
+                <div className="clock-img">
+                  <img src={Images.clock} alt="clock" />
+                </div>
+                <p className="t-1">07:21</p>
+                <p className="t-2">07:25</p>
+              </div>
             </div>
           </div>
+          <hr></hr>
           <div className="right-row-3">
             <div>
               <p>Air Quality</p>
-              <div></div>
+              <div className="half-arc">
+                <span className="label">
+                  2/5<br></br>Moderate
+                </span>
+              </div>
             </div>
             <div>
-              <p>Air Quality</p>
-              <div></div>
+              <p>UV Index</p>
+              <div className="half-arc">
+                <span className="label">
+                  6/10<br></br>high
+                </span>
+              </div>
             </div>
+            <p className="sec-1">|</p>
           </div>
         </div>
       </div>
